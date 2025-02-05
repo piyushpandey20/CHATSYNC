c9dba60740f7085be87d1849db3648bf3a04b250
Piyush Pandey <piyush2003pandey@gmail.com>
2025-02-05 11:19:12 +0530

    project completed , ready to deploy

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -52,7 +53,12 @@ export const sendMessage = async (req, res) => {
     });
 
     await newMessage.save();
-    //socket.io connection left currently
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
     res.status(200).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage: ", error.message);
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,9 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
@@ -22,7 +22,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
   connectDB();
 });
